d291c1d740d10dbe4ed9b98e04abb6c81ed4dd69
Egbuna Chinedu <78174119+enayds@users.noreply.github.com>
2023-09-10 21:51:37 +0100

    final_update

--- app.py
+++ app.py
@@ -1,45 +1,36 @@
 from flask import Flask, request, jsonify
 import datetime
-import pytz
-import time
 
 app = Flask(__name__)
 
-@app.route('/get_info', methods=['GET'])
-def get_info():
+@app.route('/api', methods=['GET'])
+def api():
     # Get query parameters
     slack_name = request.args.get('slack_name')
     track = request.args.get('track')
 
-    # Get current time in Nigeria timezone
-    timezone = pytz.timezone("Africa/Lagos")
-    current_time = datetime.datetime.now(timezone).strftime("%Y-%m-%dT%H:%M:%SZ")
-
     # Get current day of the week
     current_day = datetime.datetime.now().strftime("%A")
 
-
+    # Get current UTC time
+    utc_time = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
 
     # Construct GitHub URLs
     github_repo_url = "https://github.com/enayds/Backend_Internship"
     github_file_url = f"{github_repo_url}/blob/master/app.py"
 
-
     # Create the response JSON
     response = {
         "slack_name": slack_name,
         "current_day": current_day,
+        "utc_time": utc_time,
         "track": track,
         "github_file_url": github_file_url,
         "github_repo_url": github_repo_url,
-        "status_code": 200,
-        "current_time": current_time
+        "status_code": 200
     }
 
     return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-    
